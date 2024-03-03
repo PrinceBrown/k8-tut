ab84b68577c1d8623f414fbf94cf8778e6244635
Prince Brown <prince.brown@networkinv.com>
2024-03-02 23:21:50 -0700

    Add new ingress rule for copycat-demo-service

---
+++ 0.5.0/copycat/app.js.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('http://localhost:5400');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error occurred while fetching data');
+  }
+});
+
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
