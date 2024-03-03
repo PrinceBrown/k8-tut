f71c771d8e5b93ae5980bb0d93aa6728993c9472
Prince Brown <prince.brown@networkinv.com>
2024-03-03 01:01:16 -0700

    Update Copycat app and deployment image version, and modify client app domain

--- 0.5.0/copycat/app.js.js
+++ 0.5.0/copycat/app.js.js
@@ -6,10 +6,14 @@ const app = express();
 app.get('/', async (req, res) => {
   try {
     const response = await axios.get("http://gatewaygenius-demo-service:80");
+    response.data.serverName = "Copycat"; // Append serverName to the response data
     res.json(response.data);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error occurred while fetching data');
+    res.status(500).json({
+      message: 'A request was attempted to http://gatewaygenius-demo-service:80 but failed.',
+      error: error.response ? error.response.data : error.message
+    });
   }
 });
 
